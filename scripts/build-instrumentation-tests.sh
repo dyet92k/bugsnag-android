afe0fcfb2c7b56a1d9ae6cb60f1c53a92ef0177a
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-05-18 22:51:21 +0100

    Fix mv command

--- scripts/build-instrumentation-tests.sh
+++ scripts/build-instrumentation-tests.sh
@@ -1,5 +1,5 @@
 echo Switching to NDK $NDK_VERSION
-mv android-ndk-$NDK_VERSION $ANDROID_HOME/ndk-bundle
+mv android-ndk-$NDK_VERSION-linux-x86_64 $ANDROID_HOME/ndk-bundle
 RESULT=$?; if [ $RESULT -ne 0 ]; then exit $RESULT; fi;
 
 ./gradlew assembleAndroidTest --stacktrace
